29ff2deface2c4653f69d72f6183ab05a1cd16af
Dongsheng Li <71262244+wudi-ldd@users.noreply.github.com>
2023-12-21 15:48:59 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import QApplication,QMainWindow
 from PyQt5 import uic
 from utils.category_functions import add_category, delete_category
-from utils.file_functions import ImageViewer
+from utils.file_functions_sam import ImageViewer
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QCursor
 
@@ -19,24 +19,29 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
 
     def initUI(self):
+        # 添加类别和删除类别的按钮
         self.addButton.clicked.connect(lambda: add_category(self.listWidget))
         self.deleteButton.clicked.connect(lambda: delete_category(self.listWidget))
 
+        # 添加图片文件夹的按钮
         self.openFile.triggered.connect(lambda: self.myGraphicsViewInstance.openImageFolder())
         self.saveFile.triggered.connect(lambda: self.myGraphicsViewInstance.saveAnnotationsToFile())
 
 
+        #点击图片标注按钮
         self.pointButton.clicked.connect(lambda: self.myGraphicsViewInstance.onPointButtonClick())
 
+        #点击删除标注按钮
         self.deleteButton2.clicked.connect(lambda: self.myGraphicsViewInstance.deleteAnnotation())
 
+    #按e键位结束标注
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_E:
             self.myGraphicsViewInstance.isPointButtonClicked = False
             self.myGraphicsViewInstance.graphicsView.setCursor(QCursor(Qt.ArrowCursor))
             self.myGraphicsViewInstance.finishAnnotation()
         if event.key() == Qt.Key_Z:
-            self.myGraphicsViewInstance.undoLastPoint()  
+            self.myGraphicsViewInstance.undoLastPoint()  # 调用 ImageViewer 类中的方法撤销最后一个点
         
 
 
